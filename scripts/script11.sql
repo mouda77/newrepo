9fb07287be29139273b40744a1e68d1d4677049a
mouda77 <mmustafa8@gmail.com>
2024-01-13 23:40:17 +0200

    Add files via upload

---
+++ scripts/script11.sql
@@ -0,0 +1,9 @@
+
+CREATE TABLE student_data2 (
+           student_id int primary key,
+           student_name varchar(25),
+             student_age int,
+             gender char (1),
+             DOA date,
+             city_name varchar(20)
+ );
---
+++ scripts/script2.sql
@@ -0,0 +1,1 @@
+truncate table student_data2;
