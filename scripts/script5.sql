9c0230dbe762414022921068637e4a15e39d4c2c
mouda77 <mmustafa8@gmail.com>
2024-01-14 15:16:23 +0200

    Update and rename script4.sql to script5.sql

--- scripts/script5.sql
+++ scripts/script5.sql
@@ -3,7 +3,7 @@
 --changeset your_username:unique_id_for_changeset
 --comment: Description of your changeset
 
-create table person1 (
+create table personnnnn (
 	id int primary key,
 	name varchar(50) not null,
 	address1 varchar(50),
@@ -12,7 +12,7 @@ create table person1 (
 );
 				
 --changeset your.name:2 labels:v0 context:all
-create table company1 (
+create table companyyyy (
 	id int primary key,
 	name varchar(50) not null,
 	address1 varchar(50),
